f70e2dd9033ff253ec27ac9b74335595a1c5783d
Facu Velasco <velascofacundo@gmail.com>
2020-10-15 14:46:38 -0300

    add tailwind css

--- src/views/Index/NavBar/index.jsx
+++ src/views/Index/NavBar/index.jsx
@@ -1,151 +1,58 @@
 import React, { useState, useEffect } from "react";
-import {
-  FaHome,
-  FaSignInAlt,
-  FaSignOutAlt,
-  FaFileSignature
-} from "react-icons/fa";
-import { GiKey } from "react-icons/gi";
+import Logo from "./logo.jpg";
 import { Link, useRouteMatch } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { logOut } from "../../../store/actions";
-const styles = {
-  active: {
-    color: "black",
-    fontSize: "43px",
-    marginLeft: "8px",
-    marginTop: "8px"
-  },
-  notActive: {
-    color: "white",
-    fontSize: "40px",
-    marginLeft: "8px",
-    marginTop: "8px"
-  }
-};
+
+const Pair = ({ children }) => (
+  <div className="flex items-center justify-end space-x-8 md:flex-1 lg:w-0">
+    {children}
+  </div>
+);
+
+const EmptyButton = ({ children }) => (
+  <Link
+    to={`/${children.toLowerCase()}`}
+    className="whitespace-no-wrap text-base leading-6 font-medium text-white hover:text-gray-400 focus:outline-none focus:text-gray-400 transition ease-in-out duration-150"
+  >
+    {children}
+  </Link>
+);
+
+const RoundButton = ({ children }) => (
+  <span className="inline-flex rounded-md shadow-sm">
+    <Link
+      to={`/${children.toLowerCase()}`}
+      className="whitespace-no-wrap inline-flex items-center justify-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-purple-500 hover:bg-pink-500 focus:outline-none focus:border-purple-200 focus:shadow-outline-purple active:bg-pink-700 transition ease-in-out duration-150"
+    >
+      {children}
+    </Link>
+  </span>
+);
 
 export default () => {
-  const user = useSelector(state => state.user);
   let match = useRouteMatch({
-    path: "/secret"
+    path: "/secret",
   });
-  const dispatch = useDispatch();
-  const [active, activeChange] = useState({
-    home: false,
-    homeClick: false,
-    register: false,
-    registerClick: false,
-    logIn: false,
-    logInClick: false,
-    logOut: false,
-    logOutClick: false,
-    secret: false,
-    secretClick: false
-  });
-  const allFalse = onlytrue => {
-    const obj = JSON.parse(JSON.stringify(active));
-    for (const key in obj) {
-      key !== onlytrue ? (obj[key] = false) : (obj[key] = true);
-    }
-    activeChange(obj);
-  };
-  if (match && !active.secretClick) {
-    allFalse("secretClick");
-  }
-  const location = window.location.pathname.slice(1);
-  useEffect(() => {
-    if (location) allFalse(location + "Click");
-    else allFalse("homeClick");
-  }, [location]);
- 
 
   return (
-    <div className="navbarContainer">
-      <div className="buttonsConts">
-        <Link to="/">
-          <div
-            className={active.homeClick ? "buttonClick" : "button"}
-            onMouseEnter={() => activeChange({ ...active, home: true })}
-            onMouseLeave={() => activeChange({ ...active, home: false })}
-            onClick={() => allFalse("homeClick")}
-          >
-            <FaHome
-              style={
-                active.home || active.homeClick
-                  ? styles.active
-                  : styles.notActive
-              }
-            />
-          </div>
-        </Link>
-        <Link to="/secret">
-          <div
-            className={active.secretClick ? "buttonClick" : "button"}
-            onMouseEnter={() => activeChange({ ...active, secret: true })}
-            onMouseLeave={() => activeChange({ ...active, secret: false })}
-            onClick={() => allFalse("secretClick")}
-          >
-            <GiKey
-              style={
-                active.secret || active.secretClick
-                  ? styles.active
-                  : styles.notActive
-              }
-            />
-          </div>
-        </Link>
+    <div className="bg-purple-900">
+      <div className="flex justify-between items-center px-6 py-2">
+        <div className="w-0 flex-1">
+          <Link to="/" className="flex">
+            <img src={Logo} className="h-12 w-auto rounded-full" alt="logo" />
+          </Link>
+        </div>
+        {user ? (
+          <Pair>
+            <EmptyButton>Login</EmptyButton>
+            <RoundButton>Register</RoundButton>
+          </Pair>
+        ) : (
+          <Pair>
+            <EmptyButton>Secret</EmptyButton>
+            <RoundButton>Logout</RoundButton>
+          </Pair>
+        )}
       </div>
-
-      {!user ? (
-        <div className="buttonsConts">
-          <Link to="/register">
-            <div
-              className={active.registerClick ? "buttonClick" : "button"}
-              onMouseEnter={() => activeChange({ ...active, register: true })}
-              onMouseLeave={() => activeChange({ ...active, register: false })}
-              onClick={() => allFalse("registerClick")}
-            >
-              <FaFileSignature
-                style={
-                  active.register || active.registerClick
-                    ? styles.active
-                    : styles.notActive
-                }
-              />
-            </div>
-          </Link>
-          <Link to="/logIn">
-            <div
-              className={active.logInClick ? "buttonClick" : "button"}
-              onMouseEnter={() => activeChange({ ...active, logIn: true })}
-              onMouseLeave={() => activeChange({ ...active, logIn: false })}
-              onClick={() => allFalse("logInClick")}
-            >
-              <FaSignInAlt
-                style={
-                  active.logIn || active.logInClick
-                    ? styles.active
-                    : styles.notActive
-                }
-              />
-            </div>
-          </Link>
-        </div>
-      ) : (
-        <div className="buttonsConts">
-          <p className="personName">Hi {user.username}</p>
-          <div
-            className="button"
-            onMouseEnter={() => activeChange({ ...active, logOut: true })}
-            onMouseLeave={() => activeChange({ ...active, logOut: false })}
-            onClick={() => dispatch(logOut())}
-          >
-            <FaSignOutAlt
-              style={active.logOut ? styles.active : styles.notActive}
-            />
-          </div>
-        </div>
-      )}
     </div>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
-const path = require('path')
+const path = require("path");
 
-const appConfig = require('./app.config.js')
+const appConfig = require("./app.config.js");
 
-const REACT_DIRPATH = path.resolve(__dirname, 'src/views')
-const BUILD_DIRPATH = path.resolve(__dirname, 'src/public/javascripts/dist')
+const REACT_DIRPATH = path.resolve(__dirname, "src/views");
+const BUILD_DIRPATH = path.resolve(__dirname, "src/public/javascripts/dist");
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   devtool: false,
   entry: {
-    root: path.resolve(REACT_DIRPATH, 'Root.jsx')
+    root: path.resolve(REACT_DIRPATH, "Root.jsx"),
   },
   output: {
     path: BUILD_DIRPATH,
-    publicPath: '/javascripts/dist/',
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].bundle.js'
+    publicPath: "/javascripts/dist/",
+    filename: "[name].bundle.js",
+    chunkFilename: "[name].bundle.js",
   },
   optimization: {
     splitChunks: {
       cacheGroups: {
         commons: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-          enforce: true
-        }
-      }
-    }
+          name: "vendors",
+          chunks: "all",
+          enforce: true,
+        },
+      },
+    },
   },
   module: {
     rules: [
@@ -37,70 +37,68 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: [
-                '@babel/preset-env',
-                '@babel/preset-react'
-              ],
+              presets: ["@babel/preset-env", "@babel/preset-react"],
               plugins: [
                 [
-                  '@babel/plugin-proposal-class-properties',
+                  "@babel/plugin-proposal-class-properties",
                   {
-                    'loose': true
-                  }
+                    loose: true,
+                  },
                 ],
-                [
-                  '@babel/plugin-syntax-dynamic-import'
-                ]
-              ]
-            }
-          }
-        ]
+                ["@babel/plugin-syntax-dynamic-import"],
+              ],
+            },
+          },
+        ],
       },
       {
         test: /\.(jpe?g|png|gif)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[path][name].[ext]',
-              outputPath: '../'
-            }
-          }
-        ]
+              name: "[path][name].[ext]",
+              outputPath: "../",
+            },
+          },
+        ],
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
-              limit: '10000',
-              name: '[path][name].[ext]',
-              outputPath: '../'
-            }
-          }
-        ]
+              limit: "10000",
+              name: "[path][name].[ext]",
+              outputPath: "../",
+            },
+          },
+        ],
       },
       {
         test: /\.(css|sass|scss)$/,
         use: [
           {
-            loader: 'style-loader'
+            loader: "style-loader",
           },
           {
-            loader: 'css-loader'
+            loader: "css-loader",
           },
           {
-            loader: 'sass-loader'
-          }
-        ]
-      }
-    ]
+            loader: "sass-loader",
+          },
+          {
+            loader: "postcss-loader",
+          },
+        ],
+      },
+    ],
   },
   serve: {
     port: appConfig.port,
-    content: BUILD_DIRPATH
-  }
-}
+    content: BUILD_DIRPATH,
+  },
+};
